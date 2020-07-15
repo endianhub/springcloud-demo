09c37b91ca03512a0e11dc9928d0f98b02548197
endianhub <endianhub@users.noreply.github.com>
2020-07-15 13:18:47 +0800

    commit

---
+++ springcloud-parents-05-eurek-config/springcloud-api-service/src/main/java/com/xh/springcloud/api/service/order/OrderService.java
@@ -0,0 +1,30 @@
+package com.xh.springcloud.api.service.order;
+
+import com.xh.springcloud.common.response.ResponseBase;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Title:
+ * Description:
+ *
+ * @author H.Yang
+ * @email xhaimail@163.com
+ * @date 2020/7/14
+ */
+public interface OrderService {
+
+    // 订单服务调用会员服务接口信息 feign
+    @RequestMapping("/orderToMember")
+    public String orderToMember(String name);
+
+    // 订单服务接口调用会员服务接口
+    @RequestMapping("/orderToMemberUserInfo")
+    public ResponseBase orderToMemberUserInfo();
+
+    @RequestMapping("/orderToMemberUserInfoHystrix")
+    public ResponseBase orderToMemberUserInfoHystrix();
+
+    // 订单服务接口
+    @RequestMapping("/orderInfo")
+    public ResponseBase orderInfo();
+}
